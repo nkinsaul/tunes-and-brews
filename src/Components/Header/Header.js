7eaf19908a90c4c7a024324ce4b822a0b16b1b6b
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-03-01 12:21:48 -0800

    Add react-router-dom

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css'
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+
+  return (
+    <h1>Brew Beats</h1>
+  )
+}
+
+export default Header;
