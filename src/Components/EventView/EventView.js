0b8d5eae8b1ba9e688b24c691cd334ae6d80e9c8
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-03-02 12:29:20 -0800

    Move API call for single event to a separate file

--- src/Components/EventView/EventView.js
+++ src/Components/EventView/EventView.js
@@ -1,8 +1,7 @@
 import React, {useState, useEffect} from "react";
 import { useLocation } from "react-router-dom";
 import './EventView.css'
-const apiKey = process.env.REACT_APP_TICKETMASTERKEY
-
+import { getEvent } from "../../utilities/apiCalls";
 
 const EventView = () => {
   const [_event, setEvent] = useState({});
@@ -12,22 +11,10 @@ const EventView = () => {
   const location = useLocation();
   const event_id = location.pathname.slice(1);
 
-  const getEvent = async () => {
-  const url = `https://app.ticketmaster.com/discovery/v2/events/${event_id}.json?classificationName=music&dmaId=385&apikey=${apiKey}`
-   
-    try {
-      const response = await fetch(url)
-      const _event = await response.json()
-      setEvent(_event)
-      setLoading(false)
-      const image = _event.images.find(image => image.ratio === '16_9')
-    } catch {
-      setError(error.status)
-    }
-  }
-
   useEffect(() => {
-    getEvent()
+    getEvent(event_id)
+    .then((data) => {return setEvent(data), setLoading(false)})
+    .catch(setError(error))
   }, [])
 
   const image = _event.images?.find(image => 
@@ -37,6 +24,8 @@ const EventView = () => {
     <>
       {(loading) ? <h1>Loading...</h1> :
       <div>
+        <h1>{_event.name}</h1>
+        <h2>{_event._embedded.venues[0].name}</h2>
         <img src={image.url} />
       </div>}
     </>
