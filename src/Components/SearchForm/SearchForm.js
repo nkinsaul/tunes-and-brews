1575a421a67dce3fef69bbe000ac4b8fd69c585c
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-03-03 13:08:47 -0800

    Refactor to directly call state functions

--- src/Components/SearchForm/SearchForm.js
+++ src/Components/SearchForm/SearchForm.js
@@ -5,14 +5,6 @@ const SearchForm = () => {
   const [keyword, setKeyword] = useState('');
   const [date, setDate] = useState();
 
-  const handleKeywordChange = (event) => {
-    setKeyword(event.target.value)
-  }
-
-  const handleDateChange = (event) => {
-    setDate(event.target.value)
-  }
-
   return (
     <form className="search-form">
       <h4>Search for Events</h4>
@@ -21,14 +13,13 @@ const SearchForm = () => {
           type='search'
           placeholder="Keyword"
           value={keyword}
-          name="keyword"
-          onChange={handleKeywordChange}
+          onChange={e => setKeyword(e.target.value)}
         />
 
         <input 
           type="date"
           value={date}
-          onChange={handleDateChange}
+          onChange={e => setDate(e.target.value)}
         />
       </div>
     </form>
