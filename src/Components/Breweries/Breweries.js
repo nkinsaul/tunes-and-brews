b6e3a839d6053d030d03f963dfdf19e6b4f00a41
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-03-02 16:21:00 -0800

    Finish initial styling for single event-view

---
+++ src/Components/Breweries/Breweries.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Breweries.css'
+
+const Breweries = () => {
+  return (
+    <h1>Brewery List</h1>
+  )
+}
+
+export default Breweries
--- src/Components/EventView/EventView.js
+++ src/Components/EventView/EventView.js
@@ -1,14 +1,14 @@
 import React, {useState, useEffect} from "react";
 import { useLocation } from "react-router-dom";
-import './EventView.css'
+import './EventView.css';
 import { getEvent } from "../../utilities/apiCalls";
 import dayjs from "dayjs";
-import { Link } from "react-router-dom";
+import Breweries from "../Breweries/Breweries";
 
 const EventView = () => {
   const [_event, setEvent] = useState({});
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
   const location = useLocation();
   const event_id = location.pathname.slice(1);
@@ -25,21 +25,35 @@ const EventView = () => {
   return (
     <>
       {(loading) ? <h1>Loading...</h1> :
-      <div>
+      <div className="event-view-container">
         <div className="event-details">
-          <h1>{_event.name}</h1>
-          <h2>{_event._embedded.venues[0].name}</h2>
-          <p>{dayjs(_event.dates.start.dateTime).format('MMM/DD/YYYY')}</p>
-          <p>Ticket price range: 
-            ${Math.round(_event.priceRanges[0].min)} - 
-            ${Math.round(_event.priceRanges[0].max)}
-          </p>
-          <a href={`${_event.url}`}>Buy Tickets</a>
+          <div className="event-text">
+            <div className="event-headers">
+              <h1 className="element">{_event.name}</h1>
+              <h2 className="element">{_event._embedded.venues[0].name}</h2>
+            </div>
+            <div className="event-ticket-details">
+              <p className="element">{dayjs(_event.dates.start.dateTime).format('MMM DD, YYYY')}</p>
+              <p className="element">Ticket price range: 
+                ${Math.round(_event.priceRanges[0].min)} - 
+                ${Math.round(_event.priceRanges[0].max)}
+              </p>
+              <a href={`${_event.url}`}>Buy Tickets</a>
+             </div>
+          </div>
+          <img className='event-image' src={image.url} />
         </div>
-        <img className='event-image' src={image.url} />
+        <div className="breweries-container">
+          <Breweries 
+            postalCode={_event._embedded.venues[0].postalCode}
+          />
+        </div>
       </div>}
     </>
   )
 }
 
 export default EventView
+
+
+// event._embedded.venues[0].postalCode
